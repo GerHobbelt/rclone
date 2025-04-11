735dd1a953d6222b3ade304b90cd8ece1645d4cd
Martin Czygan <martin.czygan@gmail.com>
2025-04-11 09:52:37 +0200

    vault: fix size

--- backend/vault/vault.go
+++ backend/vault/vault.go
@@ -67,7 +67,7 @@ const (
 	// this problem go away, but there's probably a better way to handle this --
 	// would be glad to have a short in person debug session (where we can try
 	// to replicate the issue in prod together, or the like)
-	defaultUploadChunkSize = 1 * fs.Mebi
+	defaultUploadChunkSize = 1_048_576
 )
 
 func init() {
