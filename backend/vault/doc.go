9e02143b0b1c826920aa79d288074b8fc8dfaccc
Martin Czygan <martin.czygan@gmail.com>
2025-05-20 18:59:34 +0200

    vault: cleanup

--- backend/vault/doc.go
+++
@@ -1,6 +0,0 @@
-// Package vault adds support for the Vault Digital Preservation System
-// developed and hosted at the Internet Archive. Find out more at:
-// https://vault-webservices.zendesk.com/hc/en-us
-//
-//go:generate oapi-codegen -generate types,client,spec -package vault -o v2.gen.go deposit-v2-openapi.json
-package vault
